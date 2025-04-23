c29c18c29b04bf46f7faba9267c498f31355b751
Prof. Cristiano <cristiano.paula@docente.senai.br>
2025-04-23 16:31:05 -0300

    Alteração da rota de registro de usuario para a pagina complete seu cadastro

--- ProjetoLogin/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ProjetoLogin/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -108,7 +108,7 @@ namespace ProjetoLogin.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/Home/Cadastro");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
--- ProjetoLogin/Controllers/HomeController.cs
+++ ProjetoLogin/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace ProjetoLogin.Controllers
             return View();
         }
 
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
